41c7a6626e4f9375a96470f69f27e1d16e3d5188
KristiyanYordanov02 <kingroalbg@gmail.com>
2025-01-30 22:42:22 +0200

    update 30/01

--- GameStore/GameStore/obj/Debug/net8.0/GameStore.AssemblyInfo.cs
+++ GameStore/GameStore/obj/Debug/net8.0/GameStore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f780e2acecc8ce4e000d0c46453236940ffc646")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+544fa234fb3e98c05b76ee925b2f707360a02367")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
