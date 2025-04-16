1eb34e3ba9a14be1c65eba6381e4a3e191cb8e27
KristiyanYordanov02 <kingroalbg@gmail.com>
2025-04-16 20:37:52 +0300

    update

--- GameStore/GameStore/Presentation/Program.cs
+++ GameStore/GameStore/Presentation/Program.cs
@@ -37,7 +37,7 @@ using (var scope = app.Services.CreateScope())
         new Game { Id = Guid.NewGuid(), Name = "Elden Ring", Price = 59.99m, Developer = "FromSoftware" },
         new Game { Id = Guid.NewGuid(), Name = "Red Dead Redemption 2", Price = 59.99m, Developer = "Rockstar Games" },
         new Game { Id = Guid.NewGuid(), Name = "The Legend of Zelda: Breath of the Wild", Price = 59.99m, Developer = "Nintendo" },
-        new Game { Id = Guid.NewGuid(), Name = "God of War", Price = 49.99m, Developer = "Santa Monica Studio" },
+        new Game { Id = Guid.NewGuid(), Name = "God of War", Price = 39.99m, Developer = "Santa Monica Studio" },
         new Game { Id = Guid.NewGuid(), Name = "Halo Infinite", Price = 49.99m, Developer = "343 Industries" },
         new Game { Id = Guid.NewGuid(), Name = "Horizon Forbidden West", Price = 59.99m, Developer = "Guerrilla Games" },
         new Game { Id = Guid.NewGuid(), Name = "Dark Souls III", Price = 59.99m, Developer = "FromSoftware" },
